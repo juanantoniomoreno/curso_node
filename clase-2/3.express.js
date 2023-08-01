e8bc0955507a9a5445197db978b82f33f4f4fdc1
Juan Antonio Moreno <ja.morenofernadez@gmail.com>
2023-08-01 20:57:45 +0200

    express

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -2,33 +2,51 @@ const exp = require("constants");
 const express = require("express");
 const app = express();
 
+const dittoJSON = require("./pokemon/ditto");
+
+//Para quitar de la respuesta la tecnología que se ha usado
+app.disable("x-powered-by");
+
 const PORT = process.env.PORT ?? 1234;
 
+//Herramienta de express que hace lo mismo que el middleware de abajo
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//  if(req.method !== 'POST' ) return next();
-//  if(req.headers['content-type'] !== 'application/json') return next();
+// Middleware
 
-//  req.on("data", (chunk) => {
+// app.use((req, res, next) => {
+//   if (req.method !== "POST") return next();
+//   if (req.headers["content-type"] !== "application/json") return next();
+
+//   // Sólo llegan requests que son POST y el Content/Type: application/json
+
+//   let body = "";
+
+//   req.on("data", (chunk) => {
 //     body += chunk.toString();
 //   });
 
-//   req.on('end' () => {
+//   req.on("end", () => {
 //     const data = JSON.parse(body);
-//   })
+//     data.timestamp = Date.now();
 
-//   next();
+//     // Mutar la request y meter la info en el req.body
+//     req.body = data;
+//     next();
+//   });
 // });
 
-app.get("/", (req, res) => {
-  res.status(200).send("MI página");
+app.get("/pokemon/ditto", (req, res) => {
+  res.status(200).json(dittoJSON);
 });
 
-app.post("/pokemon", (req, res) => {});
+app.post("/pokemon", (req, res) => {
+  res.status(201).json(req.body);
+});
 
+//Forma global de tratar las requests
 app.use((req, res) => {
-  res.status(404).send("Error 404");
+  res.status(404).send("<h1>Error 404</h1>");
 });
 
 app.listen(PORT, () => {
