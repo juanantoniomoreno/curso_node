47281c84368141bf23222dc226347fca79df1819
Juan Antonio Moreno <ja.morenofernandez@gmail.com>
2023-08-17 20:12:17 +0200

    rest-api

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,15 +1,44 @@
 const express = require("express");
-const app = express();
-const movies = require('./movies.json');
 const crypto = require('node:crypto');
-const { validateMovie } = require("./schemas/movies");
+const movies = require('./movies.json');
+const cors = require('cors');
+const { validateMovie, validatePartialMovie } = require("./schemas/movies");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+    origin: (origin, callback) => {
+
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'https://movies.com',
+            'https://midu.dev',
+        ];
+
+        if(ACCEPTED_ORIGINS.includes(origin)){
+            return callback(null, true)
+        }
+
+        if(!origin){
+            return callback(null, true);
+        }
+
+        return callback(new Error('not-allowed-by-cors'));
+    }
+}));
 
 app.disable("x-powered-by");
 
-app.use(express.json());
-
 // Los recursos que sean MOVIES se identifican con /movies
 app.get("/movies", (req, res) => {
+
+    // const origin = req.header('origin');
+
+    // if(ACCEPTED_ORIGINS.includes(origin) || !origin ){
+    //     res.header('Access-Control-Allow-Origin', origin );        
+    // }
 
     const { genre } = req.query;
 
@@ -40,11 +69,11 @@ app.get("/movies/:id", (req, res) => { // path-to-regexp
     res.status(404).json({ message: 'movie-not-found '});
 })
 
-app.post('/movies', (req, res) => {
+app.post("/movies", (req, res) => {
 
     const result = validateMovie(req.body);
 
-    if(result.error){
+    if(!result.success){
         return res.status(400).json({ error: JSON.parse(result.error.message) });
     }
 
@@ -59,6 +88,64 @@ app.post('/movies', (req, res) => {
 
     res.status(201).json(newMovie) //actualizar cache del cliente
 });
+
+app.delete("/movies/:id", (req, res) => {
+
+    //  const origin = req.header('origin');
+
+    // if(ACCEPTED_ORIGINS.includes(origin) || !origin ){
+    //     res.header('Access-Control-Allow-Origin', origin );
+    // }
+
+    const {id} = req.params;
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if(movieIndex === -1){
+        return res.status(404).json({ message: 'movie-not-found'})
+    }
+
+    movies.splice(movieIndex, 1);
+
+    return res.json({ message: 'movie-deleted'});
+})
+
+
+app.patch("/movies/:id", (req, res) => {
+
+    const result = validatePartialMovie(req.body);
+    
+    if(!result.success){
+        return res.status(400).json({ error: JSON.parse(result.error.message)});
+    }
+    
+    const { id } = req.params;
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if(movieIndex === -1){
+        return res.status(404).json({ message: 'movie-not-found'})
+    }
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updateMovie;
+
+    return res.json(updateMovie);
+})
+
+// app.options('/movies/:id', (req, res) => {
+
+//     const origin = req.header('origin');
+
+//     if(ACCEPTED_ORIGINS.includes(origin) || !origin ){
+//         res.header('Access-Control-Allow-Origin', origin)
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+//     }
+
+//     res.send(200);
+// })
 
 const PORT = process.env.PORT ?? 1234;
 
--- clase-3/schemas/movies.js
+++ clase-3/schemas/movies.js
@@ -25,6 +25,11 @@ function validateMovie(object){
     return movieSchema.safeParse(object)
 }
 
+function validatePartialMovie(input){
+    return movieSchema.partial().safeParse(input);
+}
+
 module.exports = { 
-    validateMovie
+    validateMovie,
+    validatePartialMovie
 }
